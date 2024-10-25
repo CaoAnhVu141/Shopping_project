f3d37e7779104ec78b0e2d237e03382284acb54f
ThanhPhong1101 <banphong1101@gmail.com>
2024-10-25 16:24:03 +0700

    Hoan Thanh login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,63 +3,75 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\Customer; // Thêm import cho model Customer
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash; // Thêm import cho Hash
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     public function showLoginForm()
     {
-        return view('Front-end-Admin.auth.login'); // Tạo view cho trang đăng nhập
+        return view('Front-end-Admin.auth.login');
     }
 
     public function login(Request $request)
     {
+        // Xác thực dữ liệu đầu vào
         $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
+            'email' => [
+                'required',
+                'email',
+                'max:50',
+                'not_regex:/\s/', // Không có khoảng trắng
+                'not_regex:/\.\./', // Không có 2 dấu chấm liên tiếp
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:10', // Bắt buộc nhập tối thiểu 10 ký tự
+                'regex:/[A-Z]/', // Ít nhất 1 chữ viết hoa
+                'regex:/[a-z]/', // Ít nhất 1 chữ viết thường
+                'regex:/[\W_]/', // Ít nhất 1 ký tự đặc biệt
+                'max:50',
+            ],
+        ], [
+            'email.required' => 'Vui lòng nhập vào Email của bạn!',
+            'email.email' => 'Địa chỉ email không hợp lệ!',
+            'email.max' => 'Xin lỗi, bạn đã nhập quá giới hạn email, chúng tôi chỉ hỗ trợ tối đa cho 50 ký tự!',
+            'email.not_regex' => 'Email không được chứa khoảng trắng!',
+            'email.not_regex.2' => 'Email không được có 2 dấu chấm liên tiếp!',
+            'password.required' => 'Vui lòng nhập mật khẩu của bạn!',
+            'password.min' => 'Mật khẩu phải có ít nhất 10 ký tự!',
+            'password.regex' => 'Mật khẩu phải có ít nhất 1 chữ viết hoa, 1 chữ viết thường và 1 ký tự đặc biệt!',
+            'password.max' => 'Mật khẩu không được vượt quá 50 ký tự!',
         ]);
 
         // Kiểm tra thông tin đăng nhập
         $customer = Customer::where('email', $request->email)->first();
 
-        if ($customer && Hash::check($request->password, $customer->password)) {
-            // Nếu đăng nhập thành công, tạo session cho khách hàng
-            Auth::login($customer); // Đăng nhập người dùng
-
-            // Chuyển hướng về trang chính
-            return redirect()->intended('/demo')->with('success', 'Đăng nhập thành công!');
+        if (!$customer) {
+            // Nếu không tìm thấy email
+            return back()->withErrors([
+                'email' => 'Không tìm thấy email đã đăng ký. Nếu chưa đăng ký hãy nhấp vào đăng ký phía bên phải màn hình!',
+            ]);
         }
 
-        // Nếu đăng nhập thất bại, trả về thông báo lỗi
-        return back()->withErrors([
-            'email' => 'Thông tin đăng nhập không chính xác.',
-        ]);
-    }
+        if (!Hash::check($request->password, $customer->password)) {
+            // Nếu mật khẩu không đúng
+            return back()->withErrors([
+                'password' => 'Vui lòng kiểm tra lại mật khẩu của bạn!',
+            ]);
+        }
 
-    public function register(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:customers',
-            'password' => 'required|string|min:8|confirmed',
-        ]);
-
-        // Mã hóa và lưu thông tin khách hàng mới
-        $customer = new Customer();
-        $customer->name = $request->name;
-        $customer->email = $request->email;
-        $customer->password = Hash::make($request->password); // Mã hóa mật khẩu
-        $customer->save();
-
-        return redirect('/login')->with('success', 'Đăng ký thành công! Bạn có thể đăng nhập ngay bây giờ.');
+        // Nếu đăng nhập thành công
+        Auth::login($customer);
+        return redirect()->intended('/demo')->with('success', 'Đăng nhập thành công!');
     }
 
     public function logout(Request $request)
     {
-        Auth::logout(); // Đăng xuất
+        Auth::logout();
         return redirect('/login')->with('success', 'Bạn đã đăng xuất.');
     }
 }
--- resources/views/Front-end-Admin/auth/login.blade.php
+++ resources/views/Front-end-Admin/auth/login.blade.php
@@ -1,13 +1,156 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <title>Form</title>
+    <title>Login Form</title>
     <link href="css/style.css" type="text/css" rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Playfair+Display:wght@400;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
+
+        body {
+            background-color: #d8d8d8;
+        }
+
+        .container {
+            color: #f3f3f3;
+            display: grid;
+            place-items: center;
+            background: #f7f7f7;
+            width: 350px;
+            padding-bottom: 20px;
+            position: absolute;
+            top: 50%;
+            right: 150px;
+            transform: translate(0%, -50%);
+            margin: auto;
+            padding: 60px 60px 70px 60px;
+            font-family: verdana;
+            border-radius: 5px;
+            box-shadow: 10px 10px 0px 0px #818181;
+        }
+
+        .fl {
+            float: left;
+            width: 120px;
+            line-height: 35px;
+        }
+
+        .fontLabel {
+            color: #535353;
+        }
+
+        .fr {
+            float: right;
+        }
+
+        .clr {
+            clear: both;
+        }
+
+        .box {
+            width: 360px;
+            height: 35px;
+            margin-top: 20px;
+            font-family: verdana;
+            font-size: 12px;
+        }
+
+        .textBox:focus {
+            outline: none;
+        }
+
+        .textBox {
+            height: 35px;
+            width: 190px;
+            border: none;
+            padding-left: 20px;
+            border-radius: 3px;
+        }
+
+        .iconBox {
+            height: 35px;
+            width: 40px;
+            line-height: 38px;
+            text-align: center;
+            background: #9f9f9f;
+            border-radius: 3px;
+        }
+
+        .submit {
+            font-weight: bold;
+            border: none;
+            color: #f8f8f8;
+            width: 110px;
+            height: 35px;
+            background: #6c7ae0;
+            text-transform: uppercase;
+            cursor: pointer;
+            border-radius: 3px;
+        }
+
+        .alert {
+            padding: 10px;
+            margin: 10px 0;
+            border-radius: 5px;
+        }
+
+        .alert-success {
+            background-color: #d4edda;
+            color: #155724;
+            border: 1px solid #c3e6cb;
+        }
+
+        .alert-danger {
+            background-color: #f8d7da;
+            color: #721c24;
+            border: 1px solid #f5c6cb;
+        }
+
+        .tieude {
+            font-family: 'Montserrat', sans-serif;
+            color: #6c7ae0;
+            font-size: 50px;
+            margin-bottom: 60px;
+        }
+
+        .box-submit {
+            margin-top: 50px;
+            display: grid;
+            place-items: center;
+        }
+
+        .circle {
+            z-index: -1;
+            position: fixed;
+            background-color: white;
+            width: 2600px;
+            height: 2600px;
+            border-radius: 50%;
+            left: -125px;
+            top: 60px;
+            animation: circle 9s infinite ease-in-out;
+        }
+
+        @keyframes circle {
+            0% {
+                top: 60px;
+            }
+
+            50% {
+                top: 270px;
+            }
+
+            100% {
+                top: 60px;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -70,150 +213,3 @@
 </body>
 
 </html>
-
-<style>
-    * {
-        padding: 0;
-        margin: 0;
-    }
-
-    body {
-        background-color: #d8d8d8;
-    }
-
-    .container {
-        color: #f3f3f3;
-        display: grid;
-        place-items: center;
-        background: #f7f7f7;
-        width: 350px;
-        padding-bottom: 20px;
-        position: absolute;
-        top: 50%;
-        right: 150px;
-        transform: translate(0%, -50%);
-        margin: auto;
-        padding: 60px 60px 70px 60px;
-        font-family: verdana;
-        border-radius: 5px;
-        box-shadow: 10px 10px 0px 0px #818181;
-    }
-
-    .fl {
-        float: left;
-        width: 120px;
-        line-height: 35px;
-    }
-
-    .fontLabel {
-        color: #535353;
-    }
-
-    .fr {
-        float: right;
-    }
-
-    .clr {
-        clear: both;
-    }
-
-    .box {
-        width: 360px;
-        height: 35px;
-        margin-top: 20px;
-        font-family: verdana;
-        font-size: 12px;
-    }
-
-    .textBox:focus {
-        outline: none;
-    }
-
-    .textBox {
-        height: 35px;
-        width: 190px;
-        border: none;
-        padding-left: 20px;
-        border-radius: 3px;
-    }
-
-    .new {
-        float: left;
-    }
-
-    .iconBox {
-        height: 35px;
-        width: 40px;
-        line-height: 38px;
-        text-align: center;
-        background: #9f9f9f;
-        border-radius: 3px;
-    }
-
-
-    .submit {
-        font-weight: bold;
-        border: none;
-        color: #f8f8f8;
-        width: 110px;
-        height: 35px;
-        background: #6c7ae0;
-        text-transform: uppercase;
-        cursor: pointer;
-        border-radius: 3px;
-    }
-
-    ::-webkit-input-placeholder {
-        /* Chrome/Opera/Safari */
-
-    }
-
-    .fa {
-        display: inline-block;
-        font: normal normal normal 14px / 1 FontAwesome;
-        font-size: inherit;
-        text-rendering: auto;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        margin-top: 10px;
-        margin-bottom: auto;
-    }
-
-    .tieude {
-        font-family: 'Montserrat', sans-serif;
-        color: #6c7ae0;
-        font-size: 50;
-        margin-bottom: 60px;
-    }
-
-    .box-submit {
-        margin-top: 50px;
-        display: grid;
-        place-items: center;
-    }
-
-    .circle {
-        z-index: -1;
-        position: fixed;
-        background-color: white;
-        width: 2600px;
-        height: 2600px;
-        border-radius: 50%;
-        left: -125px;
-        top: 60px;
-        animation: circle 9s infinite ease-in-out;
-    }
-@keyframes circle {
-    0%{
-        top: 60px;
-    }
-    50%{
-        top: 270px;
-    }
-    100%
-    {
-        top: 60px;
-    }
-}
-
-</style>
