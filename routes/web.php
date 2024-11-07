e508f585a23e886ea6decffba10a0cac15e30115
ThinhEsro <le.hamthang21@gmail.com>
2024-11-07 13:51:57 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\DetailProductViewController;
 use App\Http\Controllers\AdminDashboardViewController;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\AttributeViewController;
